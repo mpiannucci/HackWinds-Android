c3b78d960cb31911a7e2397d5d4d446b9dc16f0f
Matthew Iannucci <mpiannucci@gmail.com>
2014-06-18 12:21:48 -0400

    Added cards listview to main screen

--- HackWinds/src/main/java/com/nucc/hackwinds/streamFragment.java
+++ HackWinds/src/main/java/com/nucc/hackwinds/streamFragment.java
@@ -3,7 +3,7 @@ package com.nucc.hackwinds;
 import com.nucc.hackwinds.R;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.MediaController;
@@ -11,20 +11,40 @@ import android.widget.VideoView;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 
-public class streamFragment extends Fragment {
+public class streamFragment extends ListFragment {
 
     String streamURL = "http://162.243.101.197:1935/surfcam/live.stream/playlist.m3u8";
+
+    private String conditionHeaders[] = new String[] {
+        "Overview",
+        "Wind",
+        "Swell",
+        "Tide",
+        "Conditions"
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+            R.layout.list_item, R.id.itemHeader, conditionHeaders);
+        setListAdapter(adapter);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View V = inflater.inflate(R.layout.stream_fragment, container, false);
+        View V = inflater.inflate(R.layout.current_fragment, container, false);
 
         try {
             // Create the VideoView
-            VideoView streamView = (VideoView) V.findViewById(R.id.videoStreamView);
+            VideoView streamView = (VideoView) V.findViewById(R.id.currentVideoStreamView);
 
             // Change the MediaController
             //MediaController mediaController = new MediaController(this.getActivity());
@@ -46,6 +66,6 @@ public class streamFragment extends Fragment {
         }
 
         return V;
-    }   
+    }
 
 }
