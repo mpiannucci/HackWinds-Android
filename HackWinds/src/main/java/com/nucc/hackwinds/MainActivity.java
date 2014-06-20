fb77df364d0cfa1da3ef0496d0ff9c94d0eeb725
Matthew Iannucci <mpiannucci@gmail.com>
2014-06-20 02:53:50 -0400

    removed hourly view

--- HackWinds/src/main/java/com/nucc/hackwinds/MainActivity.java
+++ HackWinds/src/main/java/com/nucc/hackwinds/MainActivity.java
@@ -5,7 +5,6 @@ import java.util.Locale;
 import com.nucc.hackwinds.R;
 import com.nucc.hackwinds.forecastFragment;
 import com.nucc.hackwinds.currentFragment;
-import com.nucc.hackWinds.hourlyFragment;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
@@ -142,9 +141,6 @@ public class MainActivity extends FragmentActivity implements
 				return new currentFragment();
 			case 1:
 				// Return the second fragment view
-				return new hourlyFragment();
-			case 2:
-				// Return the second fragment view
 				return new forecastFragment();
 			}
 			return null;
@@ -153,7 +149,7 @@ public class MainActivity extends FragmentActivity implements
 		@Override
 		public int getCount() {
 			// Show 2 total pages.
-			return 3;
+			return 2;
 		}
 
 		@Override
@@ -163,8 +159,6 @@ public class MainActivity extends FragmentActivity implements
 			case 0:
 				return getString(R.string.action_live).toUpperCase(l);
 			case 1:
-				return getString(R.string.action_48).toUpperCase(l);
-			case 2:
 				return getString(R.string.action_forecast).toUpperCase(l);
 			}
 			return null;
