7ded7cb54aadb58c0de6f9923db792b31d45908e
Matthew Iannucci <mpiannucci@gmail.com>
2014-12-19 00:00:52 -0500

    Changed buoy fragment to use the buoy model

--- HackWinds/src/main/java/com/nucc/hackwinds/BuoyFragment.java
+++ HackWinds/src/main/java/com/nucc/hackwinds/BuoyFragment.java
@@ -1,5 +1,6 @@
 package com.nucc.hackwinds;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import java.util.Date;
+import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.TimeUnit;
 import java.util.Calendar;
 import java.util.TimeZone;
@@ -24,37 +26,31 @@ import info.hoang8f.android.segmented.SegmentedGroup;
 
 
 public class BuoyFragment extends ListFragment {
+    // Public constants
+    final public int BI_LOCATION = 41;
+    final public int MTK_LOCATION = 42;
 
-    // URLs
-    final private String BI_URL = "http://www.ndbc.noaa.gov/data/realtime2/44097.txt";
-    final private String MTK_URL = "http://www.ndbc.noaa.gov/data/realtime2/44017.txt";
+    // Member variables
+    BuoyModel mBuoyModel;
+    BuoyArrayAdapter mBuoyArrayAdapter;
 
-    // Data constants
-    final private int DATA_POINTS = 20;
-    final private int DATA_HEADER_LENGTH = 38;
-    final private int DATA_LINE_LEN = 19;
-    final private int HOUR_OFFSET = 3;
-    final private int MINUTE_OFFSET = 4;
-    final private int WVHT_OFFSET = 8;
-    final private int DPD_OFFSET = 9;
-    final private int DIRECTION_OFFSET = 11;
-    final private int BI_LOCATION = 41;
-    final private int MTK_LOCATION = 42;
-
-    private double hour_offset;
+    int mLocation = BI_LOCATION;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get the buoy model
+        mBuoyModel = BuoyModel.getInstance();
+
+        // Get the BI location to initialize
+        new BackgroundBuoyAsyncTask().execute();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View V = inflater.inflate(R.layout.buoy_fragment, container, false);
-
-        // Initialize it to Block Island
-        getBuoyData(BI_LOCATION);
 
         // Set the segment control to block island
         RadioButton biButton = (RadioButton) V.findViewById(R.id.biButton);
@@ -64,85 +60,46 @@ public class BuoyFragment extends ListFragment {
         SegmentedGroup locationGroup = (SegmentedGroup) V.findViewById(R.id.segmentedBuoy);
         locationGroup.setTintColor(getResources().getColor(R.color.hackwinds_blue));
 
-        // Set the time offset variable so the times are correct
-        Calendar mCalendar = new GregorianCalendar();
-        TimeZone mTimeZone = mCalendar.getTimeZone();
-        int mGMTOffset = mTimeZone.getRawOffset();
-        hour_offset = TimeUnit.HOURS.convert(mGMTOffset, TimeUnit.MILLISECONDS);
-        if (mTimeZone.inDaylightTime(new Date())) {
-            // If its daylight savings time make fix the gmt offset
-            hour_offset++;
-        }
-
         // Set the listener for the segment group radio change
         locationGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 if (i == R.id.biButton) {
                     // Switch to block island view so get that data
-                    getBuoyData(BI_LOCATION);
+                    mLocation = BI_LOCATION;
                 } else {
-                    // Switch to Montauk view and get that data
-                    getBuoyData(MTK_LOCATION);
+                    // Switch to Montauk buoy view
+                    mLocation = MTK_LOCATION;
                 }
+                new BackgroundBuoyAsyncTask().execute();
             }
         });
-
         return V;
     }
 
-    public void getBuoyData(int location) {
-        String URL;
-        if (location == BI_LOCATION) {
-            URL = BI_URL;
-        } else {
-            URL = MTK_URL;
+    public class BackgroundBuoyAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Get the values using the model and parse the data
+            mBuoyModel.getBuoyDataForLocation(mLocation);
+
+            // Return
+            return null;
         }
 
-        Ion.with(getActivity()).load(URL).asString().setCallback(new FutureCallback<String>() {
-            @Override
-            public void onCompleted(Exception e, String result) {
-                // download done...
-                // Parse the data that was received
-                ArrayList<Buoy> buoys = parseData(result);
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
 
-                // Set the list adapter
-                BuoyArrayAdapter buoyAdapter = new BuoyArrayAdapter(getActivity(), buoys);
-                setListAdapter(buoyAdapter);
+            // Set the tide adapter to the list
+            if (mLocation == BI_LOCATION) {
+                mBuoyArrayAdapter = new BuoyArrayAdapter(getActivity(), mBuoyModel.blockIslandBuoyData);
+            } else {
+                mBuoyArrayAdapter = new BuoyArrayAdapter(getActivity(), mBuoyModel.montaukBuoyData);
             }
-        });
-    }
-
-    public ArrayList<Buoy> parseData(String rawData) {
-        // Split by the whitespace in the string
-        String[] datas = rawData.split("\\s+");
-
-        // Make an array for the buoy data
-        ArrayList<Buoy> buoyDatas = new ArrayList<Buoy>();
-
-        // Loop through the data and make new buoy objects to add to the list
-        for(int i=DATA_HEADER_LENGTH; i<(DATA_HEADER_LENGTH+(DATA_LINE_LEN*DATA_POINTS)); i+=DATA_LINE_LEN) {
-            // Create a new buoy object
-            Buoy thisBuoy = new Buoy();
-
-            // Set the time
-            thisBuoy.time = String.format("%d:%s", (Integer.valueOf(datas[i + HOUR_OFFSET])+(int)hour_offset+12)%12, datas[i + MINUTE_OFFSET]);
-
-            // Set the period and wind direction values
-            thisBuoy.dpd = datas[i+DPD_OFFSET];
-            thisBuoy.dir = datas[i+DIRECTION_OFFSET];
-
-            // Convert and set the wave height
-            String wv = datas[i+WVHT_OFFSET];
-            double h = Double.valueOf(wv) * 3.28;
-            thisBuoy.wvht = String.format("%4.2f", h);
-
-            // Save the buoy object to the list
-            buoyDatas.add(thisBuoy);
+            setListAdapter(mBuoyArrayAdapter);
         }
 
-        // Return the list of buoy objects
-        return buoyDatas;
     }
-
 }
