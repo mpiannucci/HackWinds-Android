8144556f16db8a544e87c5b193ed0319ca82d545
miannucci <miannucci@navatekltd.com>
2015-03-20 11:12:44 -0400

    Added shared prefs callback for forecast location

--- HackWinds/src/main/java/com/nucc/hackwinds/SettingsActivity.java
+++ HackWinds/src/main/java/com/nucc/hackwinds/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -21,6 +22,11 @@ import android.widget.LinearLayout;
 
 public class SettingsActivity extends ActionBarActivity {
 
+    public static final String FORECAST_LOCATION_KEY = "forecastLocation";
+    public static final String RATE_APP_KEY = "rateApp";
+    public static final String CONTACT_DEV_KEY = "contactDeveloper";
+    public static final String SHOW_DISCLAIMER_KEY = "aboutDisclaimer";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +39,7 @@ public class SettingsActivity extends ActionBarActivity {
         getFragmentManager().beginTransaction().replace(R.id.content_frame, new HackWindsPreferenceFragment()).commit();
     }
 
-    public static class HackWindsPreferenceFragment extends PreferenceFragment {
+    public static class HackWindsPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -45,9 +51,16 @@ public class SettingsActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             LinearLayout view = (LinearLayout) super.onCreateView(inflater, container, savedInstanceState);
 
+            // Get the shared preferences
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+            // Set the summary of the location preference to be the current location
+            Preference forecastLocationPref = findPreference(FORECAST_LOCATION_KEY);
+            forecastLocationPref.setSummary(sharedPrefs.getString(FORECAST_LOCATION_KEY, getResources().getStringArray(R.array.forecastLocations)[0]));
+
             // Callbacks to the preference clicks
             // First is the call to send the user Google Play to rate the app
-            Preference googlePlayPref = findPreference("rateApp");
+            Preference googlePlayPref = findPreference(RATE_APP_KEY);
             googlePlayPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
@@ -65,7 +78,7 @@ public class SettingsActivity extends ActionBarActivity {
             });
 
             // Next we want to send an email intent so they can contact me
-            Preference contactPref = findPreference("contactDeveloper");
+            Preference contactPref = findPreference(CONTACT_DEV_KEY);
             contactPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
@@ -78,7 +91,7 @@ public class SettingsActivity extends ActionBarActivity {
             });
 
             // Last we want to show a quick dialog with the apps disclaimer
-            Preference disclaimerPref = findPreference("aboutDisclaimer");
+            Preference disclaimerPref = findPreference(SHOW_DISCLAIMER_KEY);
             disclaimerPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
@@ -99,6 +112,29 @@ public class SettingsActivity extends ActionBarActivity {
             return view;
         }
 
+        @Override
+        public void onResume() {
+            super.onResume();
+            // Set up a listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences()
+                    .registerOnSharedPreferenceChangeListener(this);
+        }
 
+        @Override
+        public void onPause() {
+            super.onPause();
+            // Unregister the listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences()
+                    .unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals(FORECAST_LOCATION_KEY)) {
+                // If the location was changed set the summary to the new location
+                Preference forecastLocationPref = findPreference(key);
+                forecastLocationPref.setSummary(sharedPreferences.getString(key, getResources().getStringArray(R.array.forecastLocations)[0]));
+            }
+        }
     }
 }
