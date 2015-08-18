dbf2c6b90a162b3b20de6583476202e26644f237
Matthew Iannucci <mpiannucci@gmail.com>
2015-08-17 22:48:33 -0400

    Fix inconsistent background colors

--- HackWinds/src/main/java/com/nucc/hackwinds/views/CurrentFragment.java
+++ HackWinds/src/main/java/com/nucc/hackwinds/views/CurrentFragment.java
@@ -3,7 +3,6 @@ package com.nucc.hackwinds.views;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.AsyncTask;
